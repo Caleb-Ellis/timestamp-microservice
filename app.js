7af2f5090826abf974b33939275296c827288fc5
CEllis <tehwavy@gmail.com>
2017-05-22 14:13:53 +1000

    added moment module - app now converts unix/natural timestamps and returns to user

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
 app.use(express.static('public'));
 
 app.get('/:timeQuery', function(req, res){
   var timeQuery = req.params.timeQuery;
-  res.send('time: ' + req.params.timeQuery);
+  var date;
+
+  if (isNaN(timeQuery)) {
+    date = moment(timeQuery, 'MMMM D, YYYY');
+  } else {
+    date = moment(timeQuery, 'X');
+  }
+
+  if (date.isValid()) {
+    res.json({
+      unix: date.format('X'),
+      natural: date.format('MMMM D, YYYY')
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
 });
 
 app.listen(3000);
